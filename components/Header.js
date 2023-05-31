7b355745e314ae1b89107d750e0b7eb1e9e54d88
Grégory Marini <greg.marini@hotmail.fr>
2023-05-31 18:07:10 +0200

    styled navBar

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,25 @@ import Center from './Center';
 
 const StyledHeader = styled.header`
   background-color: #68d6da;
+  font-weight: bold;
 `;
 
 const Logo = styled(Link)`
+  color: #107275;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+`;
+
+const StyledNav = styled.nav`
+  display: flex;
+  gap: 15px;
+`;
+
+const NavLink = styled(Link)`
   color: #107275;
 `;
 
@@ -14,14 +30,16 @@ export default function Header() {
   return (
     <StyledHeader>
       <Center>
-        <Logo href={'/'}>GM_Web Ecommerce</Logo>
-        <nav>
-          <Link href={'/'}>Home</Link>
-          <Link href={'/products'}>All products</Link>
-          <Link href={'/categories'}>Categories</Link>
-          <Link href={'/account'}>Account</Link>
-          <Link href={'cart'}>Cart (0)</Link>
-        </nav>
+        <Wrapper>
+          <Logo href={'/'}>GM_Web Ecommerce</Logo>
+          <StyledNav>
+            <NavLink href={'/'}>Home</NavLink>
+            <NavLink href={'/products'}>All products</NavLink>
+            <NavLink href={'/categories'}>Categories</NavLink>
+            <NavLink href={'/account'}>Account</NavLink>
+            <NavLink href={'cart'}>Cart (0)</NavLink>
+          </StyledNav>
+        </Wrapper>
       </Center>
     </StyledHeader>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
 body{
-  color: white;
+  color: white;  
 }
 `;
 
