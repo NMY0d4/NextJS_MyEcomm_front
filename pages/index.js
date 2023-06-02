d102e78664e60a667d66f725c093fa8e5fbaa2ac
Grégory Marini <greg.marini@hotmail.fr>
2023-06-02 08:19:57 +0200

    fix bugs

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
 import Featured from '@/components/Featured';
 import Header from '@/components/Header';
+import { mongooseConnect } from '@/lib/mongoose';
+import { Product } from '@/models/Product';
 
-export default function HomePage() {
+export default function HomePage({ product }) {
   return (
     <>
       <Header />
-      <Featured />
+      <Featured product={product} />
     </>
   );
 }
 
-export function getServerSideProps() {
-  const featuredProductId = '647600560fd315f22ea770a8'
+export async function getServerSideProps() {
+  const featuredProductId = '647600560fd315f22ea770a8';
+  await mongooseConnect();
+  const data = await Product.findById(featuredProductId);
+  const product = JSON.parse(JSON.stringify(data));
+
+  return {
+    props: { product },
+  };
 }
