8be1df4c7a78abaeade22173a83ed8d91c5e8ded
Grégory Marini <greg.marini@hotmail.fr>
2023-06-07 18:51:42 +0200

    start working and sort by properties

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -2,13 +2,77 @@ import Center from '@/components/Center';
 import ProductsGrid from '@/components/ui/ProductsGrid';
 import { Category } from '@/models/Category';
 import { Product } from '@/models/Product';
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const CategoryHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  h1 {
+    font-size: 1.8rem;
+  }
+`;
+
+const FiltersWrapper = styled.div`
+  display: flex;
+  gap: 15px;
+`;
+
+const Filter = styled.div`
+  background-color: var(--tertiary);
+  padding: 5px 10px;
+  border-radius: 5px;
+  display: flex;
+  gap: 5px;
+  color: var(--primaryDark);
+  select {
+    background-color: transparent;
+    border: 0;
+    font-size: inherit;
+  }
+`;
 
 export default function CategoryPage({ category, products }) {
+  const [filtersValues, setFiltersValues] = useState(
+    category.properties.map((p) => ({ name: p.name, value: 'all' }))
+  );
+
+  function handleFilterChange(filterName, filterValue) {
+    setFiltersValues((prev) => {
+      return prev.map((p) => ({
+        name: p.name,
+        value: p.name === filterName ? filterValue : p.value,
+      }));
+    });
+  }
+
   return (
     <>
       <Center>
-        <h1>{category.name}</h1>
+        <CategoryHeader>
+          <h1>{category.name}</h1>
+          <FiltersWrapper>
+            {category.properties.map((prop) => (
+              <Filter key={prop._id}>
+                <span>{prop.name} :</span>
+                <select
+                  onChange={(e) =>
+                    handleFilterChange(prop.name, e.target.value)
+                  }
+                  value={filtersValues.find((f) => f.name === prop.name).value}
+                >
+                  <option value='all'>all</option>
+                  {prop.values.map((val) => (
+                    <option key={val} value={val}>
+                      {val}
+                    </option>
+                  ))}
+                </select>
+              </Filter>
+            ))}
+          </FiltersWrapper>
+        </CategoryHeader>
         <ProductsGrid products={products} />
       </Center>
     </>
@@ -20,6 +84,8 @@ export async function getServerSideProps(context) {
   const subCategories = await Category.find({ parent: category._id });
   const catIds = [category._id, ...subCategories.map((c) => c._id)];
   const products = await Product.find({ category: catIds });
+
+  console.log(category);
 
   return {
     props: {
